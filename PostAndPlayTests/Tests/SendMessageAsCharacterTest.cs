48e8bda6ef4bc87ed4426dfcf578b1fc878407cf
Steven Kennedy <Kennedy.Steven.P@gmail.com>
2022-10-05 08:02:01 -0600

    Setup Comment guide of steps to take for send message as character test

--- PostAndPlayTests/Tests/SendMessageAsCharacterTest.cs
+++ PostAndPlayTests/Tests/SendMessageAsCharacterTest.cs
@@ -8,5 +8,27 @@ namespace PostAndPlayTests.Tests
 {
     internal class SendMessageAsCharacterTest
     {
+        IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+
+        //Login
+        //Navigate to chats page
+        //Select and enter a chat
+        //Use dropdown to select character in chat
+        //Enter text and submit message
+        //Assert that the name includes character name
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
     }
 }
